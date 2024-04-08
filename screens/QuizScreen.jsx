a3936ac83a663d8614a1af7ff92b147aea61bfb4
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-04-08 13:19:34 -0600

    ScrollDown Indicator, and QuizScreen Play & Explore Button Alerts

--- screens/QuizScreen.jsx
+++ screens/QuizScreen.jsx
@@ -6,9 +6,16 @@ import {
   ScrollView,
   TouchableOpacity,
   Image,
+  Alert,
 } from 'react-native';
-import {RadioButton} from 'react-native-paper'; // Import RadioButton component from react-native-paper
+import {
+  useNavigation,
+  useIsFocused,
+  useFocusEffect,
+} from '@react-navigation/native';
+import {RadioButton} from 'react-native-paper';
 import quizData from '../data/quizData';
+import ScrollDownIcon from '../assets/images/ScrollDownIcon.png';
 import AnimalsIcon from '../assets/images/AnimalsIcon.png';
 import ArtIcon from '../assets/images/ArtIcon.png';
 import GeographyIcon from '../assets/images/GeographyIcon.png';
@@ -20,23 +27,48 @@ import MusicIcon from '../assets/images/MusicIcon.png';
 import ScienceIcon from '../assets/images/ScienceIcon.png';
 import SportsIcon from '../assets/images/SportsIcon.png';
 
-const QuizScreen = ({route, navigation}) => {
+const QuizScreen = ({route}) => {
+  const scrollViewRef = useRef(null);
   const {category} = route.params;
   const questions = quizData.find(item => item.category === category).questions;
   const initialSelectedOptions = new Array(questions.length).fill(-1);
-  const [selectedOptions, setSelectedOptions] = useState(
-    initialSelectedOptions,
-  );
+  const [selectedOptions, setSelectedOptions] = useState([]);
   const [state, setState] = useState('no quiz');
   const [correctCount, setCorrectCount] = useState(0);
   const [incorrectCount, setIncorrectCount] = useState(0);
   const [skippedCount, setSkippedCount] = useState(0);
-  const scrollViewRef = useRef(null);
+  const [showScrollDownIcon, setShowScrollDownIcon] = useState(false);
+  const isFocused = useIsFocused();
+  const navigation = useNavigation();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const unsubscribe = navigation.addListener('beforeRemove', e => {
+        if (state !== 'no quiz') {
+          e.preventDefault();
+          Alert.alert(
+            'Are you sure you want to leave?',
+            'All your progress will be lost.',
+            [
+              {text: 'Stay', style: 'cancel', onPress: () => {}},
+              {
+                text: 'Leave',
+                style: 'destructive',
+                onPress: () => navigation.dispatch(e.data.action),
+              },
+            ],
+          );
+        }
+      });
+
+      return unsubscribe;
+    }, [navigation, state]),
+  );
 
   const handleOptionSelect = (index, optionIndex) => {
     const updatedOptions = [...selectedOptions];
     updatedOptions[index] =
-      updatedOptions[index] === optionIndex ? -1 : optionIndex; // Toggle selection based on current selection state
+      updatedOptions[index] === optionIndex ? -1 : optionIndex;
     setSelectedOptions(updatedOptions);
   };
 
@@ -50,11 +82,11 @@ const QuizScreen = ({route, navigation}) => {
         selectedOption ===
         questions[index].options.indexOf(questions[index].correct)
       ) {
-        correct++; // Increment correct count if the selected option matches the correct index
+        correct++;
       } else if (selectedOption === -1) {
-        skipped++; // Increment skipped count if the selected option is -1 (unanswered)
+        skipped++;
       } else {
-        incorrect++; // Increment incorrect count for all other cases
+        incorrect++;
       }
     });
     setCorrectCount(correct);
@@ -62,18 +94,23 @@ const QuizScreen = ({route, navigation}) => {
     setSkippedCount(skipped);
   };
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      setSelectedOptions(initialSelectedOptions);
-      setState('started');
-      setCorrectCount(0);
-      setIncorrectCount(0);
-      setSkippedCount(0);
-      scrollViewRef.current.scrollTo({y: 0, animated: true}); // Scroll to top
-    });
+  const onScroll = event => {
+    const {layoutMeasurement, contentOffset, contentSize} = event.nativeEvent;
+    const isScrollingDown =
+      layoutMeasurement.height + contentOffset.y < contentSize.height;
+    setShowScrollDownIcon(isScrollingDown);
+  };
 
-    return unsubscribe;
-  }, [navigation]);
+  useEffect(() => {
+    if (scrollViewRef.current && scrollViewRef.current.addListener) {
+      scrollViewRef.current.addListener('scroll', onScroll);
+      return () => {
+        if (scrollViewRef.current && scrollViewRef.current.removeListener) {
+          scrollViewRef.current.removeListener('scroll', onScroll);
+        }
+      };
+    }
+  }, []);
 
   useEffect(() => {
     if (category != null) {
@@ -83,126 +120,187 @@ const QuizScreen = ({route, navigation}) => {
 
   const handleReviewPress = () => {
     setState('review');
-    scrollViewRef.current.scrollTo({y: 0, animated: true}); // Scroll to top
+    scrollViewRef.current?.scrollTo({y: 0, animated: true});
   };
 
   const handlePlayPress = () => {
-    const randomCategoryIndex = Math.floor(Math.random() * quizData.length);
-    const randomCategory = quizData[randomCategoryIndex].category;
-    setSelectedOptions(initialSelectedOptions);
-    setState('started');
-    setCorrectCount(0);
-    setIncorrectCount(0);
-    setSkippedCount(0);
-    navigation.navigate('Quiz', {category: randomCategory});
-    scrollViewRef.current.scrollTo({y: 0, animated: true}); // Scroll to top
+    if (state !== 'no quiz') {
+      Alert.alert(
+        'Are you sure you want to leave?',
+        'All your progress will be lost.',
+        [
+          {text: 'Stay', style: 'cancel', onPress: () => {}},
+          {
+            text: 'Leave',
+            style: 'destructive',
+            onPress: () => {
+              const randomCategoryIndex = Math.floor(
+                Math.random() * quizData.length,
+              );
+              const randomCategory = quizData[randomCategoryIndex].category;
+              setSelectedOptions(initialSelectedOptions);
+              setState('started');
+              setCorrectCount(0);
+              setIncorrectCount(0);
+              setSkippedCount(0);
+              navigation.navigate('Quiz', {category: randomCategory});
+              if (scrollViewRef.current) {
+                scrollViewRef.current.scrollTo({y: 0, animated: true});
+              }
+            },
+          },
+        ],
+      );
+    } else {
+      const randomCategoryIndex = Math.floor(Math.random() * quizData.length);
+      const randomCategory = quizData[randomCategoryIndex].category;
+      setSelectedOptions(initialSelectedOptions);
+      setState('started');
+      setCorrectCount(0);
+      setIncorrectCount(0);
+      setSkippedCount(0);
+      navigation.navigate('Quiz', {category: randomCategory});
+      if (scrollViewRef.current) {
+        scrollViewRef.current.scrollTo({y: 0, animated: true});
+      }
+    }
   };
 
   const handleExplorePress = () => {
-    navigation.navigate('Category');
+    if (state !== 'no quiz') {
+      Alert.alert(
+        'Are you sure you want to leave?',
+        'All your progress will be lost.',
+        [
+          {text: 'Stay', style: 'cancel', onPress: () => {}},
+          {
+            text: 'Leave',
+            style: 'destructive',
+            onPress: () => navigation.navigate('Category'),
+          },
+        ],
+      );
+    } else {
+      navigation.navigate('Category');
+    }
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container} ref={scrollViewRef}>
-      {state == 'no quiz' && (
-        <Text style={styles.category}>No quiz selected</Text>
-      )}
-      {state != 'no quiz' && (
-        <View style={styles.header}>
-          <Image source={getCategoryIcon(category)} style={styles.icon} />
-          <Text style={styles.category}>{category} Quiz</Text>
-        </View>
-      )}
-      {(state == 'started' || state == 'review') &&
-        questions.map((question, index) => (
-          <View key={index} style={styles.questionWrapper}>
-            <View style={styles.questionContainer}>
-              <Text style={styles.questionText}>{question.question}</Text>
-              {question.options.map((option, optionIndex) => {
-                const selected = selectedOptions[index] === optionIndex;
-                const isCorrect = option === question.correct;
-                const isIncorrect = selected && !isCorrect;
-                const isUnanswered = !selected && !(state == 'review');
-                const isSkipped =
-                  !selected && state == 'review' && option === question.correct;
-                return (
-                  <TouchableOpacity
-                    key={optionIndex}
-                    style={[
-                      styles.optionContainer,
-                      state == 'review' &&
-                      selectedOptions[index] === optionIndex &&
-                      isCorrect
-                        ? styles.correctOptionContainer
-                        : state == 'review' &&
-                          selectedOptions[index] === optionIndex &&
-                          isIncorrect
-                        ? styles.incorrectOptionContainer
-                        : isUnanswered
-                        ? styles.unansweredOptionContainer
-                        : isSkipped
-                        ? styles.skippedOptionContainer
-                        : null,
-                    ]}
-                    onPress={() => handleOptionSelect(index, optionIndex)}
-                    disabled={state == 'review'}>
-                    <RadioButton
-                      value={optionIndex}
-                      status={selected ? 'checked' : 'unchecked'}
-                      onPress={() => handleOptionSelect(index, optionIndex)}
-                      disabled={state == 'review'}
-                      color="#FFFFFF"
-                    />
-                    <Text style={styles.optionText}>{option}</Text>
-                  </TouchableOpacity>
-                );
-              })}
-            </View>
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContentContainer}
+        onScroll={onScroll}
+        scrollEventThrottle={16}
+        ref={scrollViewRef}>
+        {state == 'no quiz' && (
+          <Text style={styles.category}>No quiz selected</Text>
+        )}
+        {state != 'no quiz' && (
+          <View style={styles.header}>
+            <Image source={getCategoryIcon(category)} style={styles.icon} />
+            <Text style={styles.category}>{category} Quiz</Text>
           </View>
-        ))}
-      {state == 'started' && (
-        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-          <Text style={styles.submitButtonText}>Submit</Text>
-        </TouchableOpacity>
-      )}
-      {(state == 'submitted' || state == 'review') && (
-        <View style={styles.summaryContainer}>
-          <Text style={styles.summaryText}>
-            Your performance on this {category} quiz was:
-          </Text>
-          <Text style={styles.summaryText}>
-            Correct Answers: {correctCount}
-          </Text>
-          <Text style={styles.summaryText}>
-            Incorrect Answers: {incorrectCount}
-          </Text>
-          <Text style={styles.summaryText}>
-            Skipped Questions: {skippedCount}
-          </Text>
-          {state == 'submitted' && (
+        )}
+        {(state == 'started' || state == 'review') &&
+          questions.map((question, index) => (
+            <View key={index} style={styles.questionWrapper}>
+              <View style={styles.questionContainer}>
+                <Text style={styles.questionText}>{question.question}</Text>
+                {question.options.map((option, optionIndex) => {
+                  const selected = selectedOptions[index] === optionIndex;
+                  const isCorrect = option === question.correct;
+                  const isIncorrect = selected && !isCorrect;
+                  const isUnanswered = !selected && !(state == 'review');
+                  const isSkipped =
+                    !selected &&
+                    state == 'review' &&
+                    option === question.correct;
+                  return (
+                    <TouchableOpacity
+                      key={optionIndex}
+                      style={[
+                        styles.optionContainer,
+                        state == 'review' &&
+                        selectedOptions[index] === optionIndex &&
+                        isCorrect
+                          ? styles.correctOptionContainer
+                          : state == 'review' &&
+                            selectedOptions[index] === optionIndex &&
+                            isIncorrect
+                          ? styles.incorrectOptionContainer
+                          : isUnanswered
+                          ? styles.unansweredOptionContainer
+                          : isSkipped
+                          ? styles.skippedOptionContainer
+                          : null,
+                      ]}
+                      onPress={() => handleOptionSelect(index, optionIndex)}
+                      disabled={state == 'review'}>
+                      <RadioButton
+                        value={optionIndex}
+                        status={selected ? 'checked' : 'unchecked'}
+                        onPress={() => handleOptionSelect(index, optionIndex)}
+                        disabled={state == 'review'}
+                        color="#FFFFFF"
+                      />
+                      <Text style={styles.optionText}>{option}</Text>
+                    </TouchableOpacity>
+                  );
+                })}
+              </View>
+            </View>
+          ))}
+        {state == 'started' && (
+          <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+            <Text style={styles.submitButtonText}>Submit</Text>
+          </TouchableOpacity>
+        )}
+        {(state == 'submitted' || state == 'review') && (
+          <View style={styles.summaryContainer}>
+            <Text style={styles.summaryText}>
+              Your performance on this {category} quiz was:
+            </Text>
+            <Text style={styles.summaryText}>
+              Correct Answers: {correctCount}
+            </Text>
+            <Text style={styles.summaryText}>
+              Incorrect Answers: {incorrectCount}
+            </Text>
+            <Text style={styles.summaryText}>
+              Skipped Questions: {skippedCount}
+            </Text>
+            {state == 'submitted' && (
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={[styles.button, styles.reviewButton]}
+                  onPress={handleReviewPress}>
+                  <Text style={styles.buttonText}>Review</Text>
+                </TouchableOpacity>
+              </View>
+            )}
             <View style={styles.buttonContainer}>
               <TouchableOpacity
-                style={[styles.button, styles.reviewButton]}
-                onPress={handleReviewPress}>
-                <Text style={styles.buttonText}>Review</Text>
+                style={[styles.button, styles.playButton]}
+                onPress={handlePlayPress}>
+                <Text style={styles.buttonText}>Play</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, styles.exploreButton]}
+                onPress={handleExplorePress}>
+                <Text style={styles.buttonText}>Explore</Text>
               </TouchableOpacity>
             </View>
-          )}
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-              style={[styles.button, styles.playButton]}
-              onPress={handlePlayPress}>
-              <Text style={styles.buttonText}>Play</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.button, styles.exploreButton]}
-              onPress={handleExplorePress}>
-              <Text style={styles.buttonText}>Explore</Text>
-            </TouchableOpacity>
           </View>
-        </View>
-      )}
-    </ScrollView>
+        )}
+      </ScrollView>
+      <View
+        style={[
+          styles.scrollDownContainer,
+          {display: showScrollDownIcon ? 'flex' : 'none'},
+        ]}>
+        <Image source={ScrollDownIcon} style={styles.scrollDownIcon} />
+      </View>
+    </View>
   );
 };
 
@@ -239,6 +337,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#0569B0',
     padding: 20,
+    position: 'relative',
+  },
+  scrollContentContainer: {
+    width: '100%',
+  },
+  scrollDownContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  scrollDownIcon: {
+    width: 40,
+    height: 40,
   },
   header: {
     flexDirection: 'column',
@@ -305,8 +416,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     paddingVertical: 10,
     paddingHorizontal: 20,
-    borderRadius: 5,
+    borderRadius: 29,
     marginTop: 20,
+    alignSelf: 'center',
   },
   submitButtonText: {
     color: '#FFFFFF',
