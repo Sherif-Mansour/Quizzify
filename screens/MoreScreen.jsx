12cd55d589f2ab0ef134b0bf0b8b8ce269b55cdd
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-04-08 13:47:41 -0600

    Implement MoreScreen

--- screens/MoreScreen.jsx
+++ screens/MoreScreen.jsx
@@ -5,7 +5,27 @@ const MoreScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>More Screen</Text>
-      <Text style={styles.text}>Placeholder content for the More screen.</Text>
+      <View style={[styles.infoContainer, styles.sectionBackground]}>
+        <Text style={styles.infoTitle}>About Us</Text>
+        <Text style={styles.infoText}>
+          Welcome to Quizzify – the ultimate destination for trivia enthusiasts!
+          Embark on a thrilling journey of knowledge and entertainment with our
+          diverse range of quizzes covering topics from history and geography to
+          music and sports. With Quizzify, learning is fun and engaging, making
+          it the perfect companion for both casual gamers and avid learners. Get
+          ready to quiz, learn, and repeat with Quizzify – where knowledge meets
+          excitement!
+        </Text>
+      </View>
+      <View style={[styles.infoContainer, styles.sectionBackground]}>
+        <Text style={styles.infoTitle}>Version</Text>
+        <Text style={styles.infoText}>1.0.0</Text>
+      </View>
+      <View style={[styles.infoContainer, styles.sectionBackground]}>
+        <Text style={styles.infoTitle}>Contact</Text>
+        <Text style={styles.infoText}>Email: smart@mergelabs.com</Text>
+        <Text style={styles.infoText}>Phone: +403-284-7248</Text>
+      </View>
     </View>
   );
 };
@@ -15,16 +35,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#0569B0',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  text: {
-    fontSize: 18,
+  infoContainer: {
+    marginBottom: 20,
+    width: '90%',
+  },
+  infoTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  infoText: {
+    fontSize: 16,
     textAlign: 'center',
+  },
+  sectionBackground: {
+    backgroundColor: '#47B2FF',
+    borderRadius: 10,
+    padding: 10,
   },
 });
 
